721359f531e3eee6daeaa4dc54ff368d2d38313d
gabrielexposito13 <ge1316a@american.edu>
2023-05-30 00:24:10 -0400

    Create Early Concepts.py

---
+++ Early Concepts.py
@@ -0,0 +1,34 @@
+#Testing Repository
+
+#Variables
+x = 1
+x = x + 12
+print(x)
+
+#Strings
+x = "Sher"
+y = "bert"
+cat = x + y
+cat 
+
+cat = cat[4:8]
+cat
+
+#Functions
+def f(x):
+    y = x+5
+    return y
+f(3)
+
+#If Statements
+x = 15
+if x+1 == 6 or x < 10:
+    print("Correct")
+else: 
+    print("Incorrect")
+
+#Loops -- Printing multiples of 3 in range 1:25
+for i in range(1,25):
+    if i%3 == 0:
+        print(i)
+
