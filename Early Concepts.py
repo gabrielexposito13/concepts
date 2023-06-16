36acf70c6eb258929fe79efe50ad575c0d69038c
gabrielexposito13 <ge1316a@american.edu>
2023-06-16 19:15:26 -0400

    Couple of concepts on concatenation

--- Early Concepts.py
+++ Early Concepts.py
@@ -32,3 +32,5 @@ for i in range(1,25):
     if i%3 == 0:
         print(i)
 
+
+# Inheritance
---
+++ Name Generator.py
@@ -0,0 +1,8 @@
+#Band Name Generator 
+
+print("This is a book title generator.")
+city = input("What city were you born in? ")
+pet = input("What is the name of your pet? ")
+print("The name of your band is "+ city + " " + pet)
+input()
+
